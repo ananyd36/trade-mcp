f8d070a47c9eda511c47f689370b9a5573fc8ce4
ananyd36 <ananyd36@gmail.com>
2025-07-06 13:49:46 +0530

    updated script/zerodha

--- scripts/zerodha.js
+++ scripts/zerodha.js
@@ -1,37 +1,44 @@
-// import { KiteConnect } from "kiteconnect";
-import { GoogleGenAI } from "@google/genai";
+import { KiteConnect } from "kiteconnect";
 
+const apiKey = "";
+const apiSecret =  "";
+const requestToken =  "";
+const access_token = "";
 
-// const apiKey = "saxoq5yc86yfkla9";
-// const apiSecret = "cuzqwpkn05mbteq6dg58yaum6rdeh6zj"
-// const access_token = "i7i6mviu518YRmK4pP6IgjcqX1j5tLEi";
-const gemini_api_key = "AIzaSyC9RcGRQ_Myk544etFx7qAwerhNH9sh7vY";
-
-const ai = new GoogleGenAI({apiKey : gemini_api_key});
-
-
-
-// const kc = new KiteConnect({ api_key: apiKey });
-
+const kc = new KiteConnect({ api_key: apiKey });
+console.log(kc.getLoginURL());
+kc.setAccessToken(access_token);
 
 async function init() {
   try {
-    await findingStocks();
+    // await generateSession();
+    await getProfile();
   } catch (err) {
     console.error(err);
+
   }
 }
 
-// async function generateSession() {
-//   try {
-//     const response = await kc.generateSession("3coJ5y76HPx0KQjtYUm5aPyNvthhX456", apiSecret);
-//     kc.setAccessToken(response.access_token);
-//     console.log(response.access_token)
-//     console.log("Session generated:", response);
-//   } catch (err) {
-//     console.error("Error generating session:", err);
-//   }
-// }
+async function generateSession() {
+  try {
+    const response = await kc.generateSession(requestToken, apiSecret);
+    console.log(response.access_token);
+    kc.setAccessToken(access_token);
+    console.log("Session generated:", response);
+  } catch (err) {
+    console.error("Error generating session:", err);
+  }
+}
+
+async function getProfile() {
+  try {
+    const profile = await kc.getTrades();
+    console.log("Profile:", profile);
+  } catch (err) {
+    console.error("Error getting profile:", err);
+  }
+}
+
 
 export async function findingStocks(){
 
@@ -78,6 +85,5 @@ export async function findingStocks(){
       return [];
   }
 }
-
-// // Initialize the API calls
+// Initialize the API calls
 init();
